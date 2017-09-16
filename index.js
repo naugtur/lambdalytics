340fa6897ef3bd4f2ea958d950b55da97ce55466
Naugtur <naugtur@gmail.com>
2017-09-16 11:38:33 +0200

    add basic geoip

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const { MONGO_URL } = process.env
 const db = require('mongojs')(MONGO_URL, ['lambdalyticsCount'])
 const app = require('express')()
 const requestIp = require('request-ip')
+const got = require('got')
 
 app.use(requestIp.mw())
 
 app.get('/', (req, res) => {
   const ref = req.headers.referer
-  if (ref) {
-    db.lambdalyticsCount.save({
-      site: ref,
-      ip: req.clientIp || 'unknown',
-      time: Date.now()
-    })
+  if (true) {
+    got(`freegeoip.net/json/${req.clientIp}`)
+        .then(({body}) => {
+          body = JSON.parse(body)
+          const location = body.country_code + '/' + body.city
+          db.lambdalyticsCount.save({
+            site: ref,
+            ip: req.clientIp || 'unknown',
+            location,
+            time: Date.now()
+          })
+        })
   }
   res.set('cache-control', 'max-age=600')
   res.end()
